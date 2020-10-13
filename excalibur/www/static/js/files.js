b0aca89e5d8f31865a00cfdbfd821b3bac59320a
OPT\jbwesterberg <1150447@isep.ipp.pt>
2020-10-13 13:04:53 +0100

    Changed search to regex

--- excalibur/www/static/js/files.js
+++ excalibur/www/static/js/files.js
@@ -38,9 +38,9 @@ $(document).ready(function () {
   });
 
   $("#search").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
+    var regex = new RegExp($(this).val().toLowerCase());
     $("#filtertable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      $(this).toggle(regex.test($(this).text().toLowerCase()))
     });
   });
 });
